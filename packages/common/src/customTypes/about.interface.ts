b8f4914a9e01229cb20929dac7fdbfae0bd84d44
Clement-Z4RM <clement.lienard@epitech.eu>
2024-10-30 21:17:32 +0100

    feat(common): move `Services` type from `frontend` to `common` and add other services-related types

---
+++ packages/common/src/customTypes/about.interface.ts
@@ -0,0 +1,12 @@
+import { AboutJson } from "../types/about/interfaces/about.interface";
+
+// TODO: use them everywhere
+
+export type Services = AboutJson["server"]["services"];
+export type Service = Services[number];
+
+export type Actions = Service["actions"];
+export type Action = Actions[number];
+
+export type Reactions = Service["reactions"];
+export type Reaction = Reactions[number];
