1d963b0078fa047fc57e75e802929c49c420f254
DURAND Malo <malo.durand@epitech.eu>
2024-11-02 12:54:41 +0100

    test(backend): adding tests for oauth and prisma.

--- apps/backend/src/oauth/oauth.service.ts
+++ apps/backend/src/oauth/oauth.service.ts
@@ -5,7 +5,7 @@ import {
     InternalServerErrorException,
     UnprocessableEntityException
 } from "@nestjs/common";
-import { hash } from "node:crypto";
+import * as crypto from "node:crypto";
 import { Cache } from "cache-manager";
 import { User } from "../users/interfaces/user.interface";
 import { ForbiddenException } from "@nestjs/common";
@@ -32,7 +32,7 @@ export class OAuthService {
     ) {}
 
     private static createState(userId: User["id"], requestedAt: number) {
-        return hash("SHA-512", `${userId}:${requestedAt}`, "hex");
+        return crypto.hash("SHA-512", `${userId}:${requestedAt}`, "hex");
     }
 
     private async prepareOAuthSession(
@@ -194,7 +194,7 @@ export class OAuthService {
             client_secret: provider.CLIENT_SECRET,
             token,
             token_type_hint: tokenTypeHint
-        });
+        }).toString();
 
         try {
             await axios.post(provider.OAUTH_REVOKE_URL, data, {
@@ -205,7 +205,7 @@ export class OAuthService {
         } catch (e) {
             const { error } = e.response.data;
 
-            if ("invalid_client" === error) {
+            if (400 === e.response.status) {
                 throw new UnprocessableEntityException(
                     "Unable to revoke the token. It may be from the wrong provider."
                 );
