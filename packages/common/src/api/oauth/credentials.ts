b6d6ad90781616fdcc3da17b718ccb40d9217bff
Clement-Z4RM <clement.lienard@epitech.eu>
2024-11-02 02:39:11 +0100

    fix(common-api): change/add response status codes for OAuth request to fit with the backend changes (PR #151)

--- packages/common/src/api/oauth/credentials.ts
+++ packages/common/src/api/oauth/credentials.ts
@@ -1,5 +1,5 @@
 import type { RequestResponse } from "../api";
-import type { Service } from "../types/Service";
+import type { OAuthService } from "../types/OAuthService";
 
 type OAuthCredentialsResponse = {
     id: number
@@ -9,7 +9,7 @@ type OAuthCredentialsResponse = {
     scope: string
 }
 
-export default async function credentials(apiUrl: string, service: Service, accessToken: string): Promise<RequestResponse<OAuthCredentialsResponse, 200 | 401>> {
+export default async function credentials(apiUrl: string, service: OAuthService, accessToken: string): Promise<RequestResponse<OAuthCredentialsResponse, 200 | 401 | 404>> {
     try {
         const response = await fetch(`${apiUrl}/oauth/${service}/credentials`, {
             method: "GET",
@@ -22,6 +22,8 @@ export default async function credentials(apiUrl: string, service: Service, acce
             return { status: 200, success: true, body: await response.json() };
         case 401:
             return { status: 401, success: false }; // This route is protected. The client must supply a Bearer token.
+        case 404:
+            return { status: 404, success: false }; // The specified provider is not supported.
         default:
             console.error(response);
             return { status: 500, success: false };
