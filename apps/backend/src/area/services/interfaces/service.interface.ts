81a08b11c347ef61ae6562608ec4c6fb883d0fcc
ElouanR <elouan.rigomont@epitech.eu>
2024-11-03 17:49:49 +0100

    feat(backend/twitch/areas): add Twitch AREAS

--- apps/backend/src/area/services/interfaces/service.interface.ts
+++ apps/backend/src/area/services/interfaces/service.interface.ts
@@ -2,9 +2,11 @@ import {
     AreaYouTubeSubscriber,
     AreaYouTubeVideo
 } from "../youtube/interfaces/youtube-video.interface";
+import { AreaTwitchStream } from "../twitch/interfaces/twitch-stream.interface";
+import { AreaTwitchFollower } from "../twitch/interfaces/twitch-follower.interface";
 
 export interface ActionResource {
-    data: AreaYouTubeVideo | AreaYouTubeSubscriber | null;
+    data: AreaYouTubeVideo | AreaYouTubeSubscriber | AreaTwitchStream | AreaTwitchFollower | string | null;
     cacheValue: string | null;
 }
 
@@ -12,7 +14,7 @@ export interface ActionDescription {
     description: string;
     oauthScopes: string[];
     oauthProvider: string;
-    trigger: (accessToken: string) => Promise<ActionResource>;
+    trigger: (accessToken: string, data: object) => Promise<ActionResource>;
 }
 
 export interface ReactionDescription {
