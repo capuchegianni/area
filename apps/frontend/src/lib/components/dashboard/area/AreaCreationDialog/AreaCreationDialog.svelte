9b82bcacbec5747999dad2951fbcb37afb764155
Clement-Z4RM <clement.lienard@epitech.eu>
2024-11-03 18:03:04 +0100

    feat(frontend-area-creation): finish OAuth for Actions and REActions
    Add an `AREActionSelection` component that permits to select a (RE)Action, and select an existant OAuth (or "create" a new connecting to the service).
    The selected (RE)Action and OAuth are preserved (in session storage) during OAuth flow, and the success status, service and OAuth id is sent to the frontend through search params.

--- apps/frontend/src/lib/components/dashboard/area/AreaCreationDialog/AreaCreationDialog.svelte
+++ apps/frontend/src/lib/components/dashboard/area/AreaCreationDialog/AreaCreationDialog.svelte
@@ -1,16 +1,12 @@
 <!-- TODO: i18n -->
 
 <script lang="ts">
-    import { onMount, afterUpdate } from "svelte";
     import type { PageServerData, ActionData } from "../../../../../routes/[lang=lang]/dashboard/$types";
-    import type { Services, Action, Reaction } from "@common/area/types/area";
+    import type { Services } from "@common/area/types/area";
     import {  buttonVariants } from "$lib/components/ui/button";
     import * as Dialog from "$lib/components/ui/dialog";
-    import Combobox from "$lib/components/dashboard/area/Combobox/Combobox.svelte";
     import LL from "$i18n/i18n-svelte";
     import AreaForm from "./AreaForm.svelte";
-    import servicesItemsToChoices from "$lib/utils/dashboard/servicesItemsToChoices";
-    import { Separator } from "$lib/components/ui/separator";
 
     export let onOpenChange: (open: boolean) => unknown;
 
@@ -19,25 +15,6 @@
     export let oauthResult: PageServerData["oauthResult"];
 
     export let form: ActionData;
-
-    const actions: Record<string, Action> = {};
-    const reactions: Record<string, Reaction> = {};
-
-    const actionsChoices = servicesItemsToChoices(services, "actions", actions);
-    const reactionsChoices = servicesItemsToChoices(services, "reactions", reactions);
-
-    let actionId: string = "";
-    let reactionId: string = "";
-
-    onMount(() => {
-        actionId = sessionStorage.getItem("actionId") || "";
-        reactionId = sessionStorage.getItem("reactionId") || "";
-    });
-
-    afterUpdate(() => {
-        sessionStorage.setItem("actionId", actionId);
-        sessionStorage.setItem("reactionId", reactionId);
-    });
 </script>
 
 <Dialog.Root open={oauthResult.success !== null} onOpenChange={onOpenChange}>
@@ -51,21 +28,11 @@
                 {$LL.area.createAreaDescription()}
             </Dialog.Description>
         </Dialog.Header>
-        <div class="grid gap-4 py-4">
-            <Combobox title="Action" choices={actionsChoices} value={actionId} setValue={(value) => actionId = value} />
-            <Combobox title="REAction" choices={reactionsChoices} value={reactionId} setValue={(value) => reactionId = value} />
-            {#if actionId && reactionId}
-                <Separator />
-                <AreaForm
-                    action={actions[actionId]}
-                    actionId={actionId}
-                    reaction={reactions[reactionId]}
-                    reactionId={reactionId}
-                    oauthCredentials={oauthCredentials}
-                    oauthResult={oauthResult}
-                    form={form}
-                />
-            {/if}
-        </div>
+        <AreaForm
+            services={services}
+            oauthCredentials={oauthCredentials}
+            oauthResult={oauthResult}
+            form={form}
+        />
     </Dialog.Content>
 </Dialog.Root>
--- apps/frontend/src/lib/components/dashboard/area/AreaCreationDialog/AreaForm.svelte
+++ apps/frontend/src/lib/components/dashboard/area/AreaCreationDialog/AreaForm.svelte
@@ -3,16 +3,19 @@
 <script lang="ts">
     import type { HTMLInputTypeAttribute } from "svelte/elements";
     import type { PageServerData, ActionData } from "../../../../../routes/[lang=lang]/dashboard/$types";
-    import { applyAction, enhance } from "$app/forms";
-    import type { Action, Reaction } from "@common/area/types/area";
+    import { enhance } from "$app/forms";
+    import type { Services, Action, Reaction } from "@common/area/types/area";
     import { Label } from "$lib/components/ui/label";
     import { Input } from "$lib/components/ui/input";
     import { Separator } from "$lib/components/ui/separator";
     import { ScrollArea } from "$lib/components/ui/scroll-area";
     import { Button } from "$lib/components/ui/button";
     import LL from "$i18n/i18n-svelte";
-    import { serviceName, oauthServiceName } from "@common/area/services";
     import Combobox from "$lib/components/dashboard/area/Combobox/Combobox.svelte";
+    import { Switch } from "$lib/components/ui/switch";
+    import servicesItemsToChoices from "$lib/utils/dashboard/servicesItemsToChoices";
+    import { onMount } from "svelte";
+    import AREActionSelection from "$lib/components/dashboard/area/AreaCreationDialog/AREActionSelection.svelte";
 
     type Field = {
         name: string;
@@ -100,106 +103,162 @@
         ]
     };
 
-    export let action: Action;
-    // eslint-disable-next-line svelte/valid-compile
-    export let actionId: string;
-    export let reaction: Reaction;
-    export let reactionId: string;
+    export let services: Services;
     export let oauthCredentials: PageServerData["oauthCredentials"];
     export let oauthResult: PageServerData["oauthResult"];
 
     export let form: ActionData;
 
-    $: service = oauthServiceName(action.serviceName);
+    const actions: Record<string, Action> = {};
+    const reactions: Record<string, Reaction> = {};
 
-    $: oauthCredentialChoices = oauthCredentials[service]?.map(credential => ({
+    const actionsChoices = servicesItemsToChoices(services, "actions", actions);
+    const reactionsChoices = servicesItemsToChoices(services, "reactions", reactions);
+
+    let actionId: string = "";
+    let reactionId: string = "";
+    let lastUpdated: "action" | "reaction";
+
+    $: actionService = actions[actionId]?.oauthProvider;
+    $: reactionService = reactions[reactionId]?.oauthProvider;
+
+    $: actionOAuthCredentialChoices = oauthCredentials[actionService]?.map(credential => ({
+        value: credential,
+        label: credential
+    })) || [];
+    $: reactionOAuthCredentialChoices = oauthCredentials[reactionService]?.map(credential => ({
         value: credential,
         label: credential
     })) || [];
 
-    let oauthId: string = "";
+    let oauthIds = {
+        action: "",
+        reaction: ""
+    };
 
-    $: if (oauthResult.id !== null)
-        oauthId = oauthResult.id;
+    onMount(() => {
+        actionId = sessionStorage.getItem("actionId") || "";
+        reactionId = sessionStorage.getItem("reactionId") || "";
+        oauthIds.action = sessionStorage.getItem("actionOAuthId") || "";
+        oauthIds.reaction = sessionStorage.getItem("reactionOAuthId") || "";
+
+        const __lastUpdated = sessionStorage.getItem("lastUpdated") || "";
+        if (__lastUpdated === "action" || __lastUpdated === "reaction") {
+            lastUpdated = __lastUpdated;
+            if (oauthResult.id !== null) {
+                oauthIds[lastUpdated] = oauthResult.id;
+                sessionStorage.setItem(`${lastUpdated}OAuthId`, oauthResult.id);
+            }
+        }
+    });
+
+    const setLastUpdated = (value: "action" | "reaction") => {
+        lastUpdated = value;
+        sessionStorage.setItem("lastUpdated", value);
+        oauthResult = {
+            success: null,
+            service: null,
+            id: null
+        };
+    }
 </script>
 
-<ScrollArea class="max-h-96">
-    {#if action.auth === "oauth"}
-        <!-- TODO: fix width/décalage -->
-        <Combobox title="OAuth" choices={oauthCredentialChoices} value={oauthId} setValue={(value) => oauthId = value} />
-        <p class="text-center py-2">ou</p>
-        <form
-            method="POST"
-            action="?/oauth"
-            use:enhance={async ({ formData }) => {
-                formData.set("service", service);
-                formData.set("redirect-uri", `${window.location.origin}/oauth/${service}/callback`);
-                formData.set("scope", action.oauthScopes?.join(" ") || "");
-                return async ({ result }) => await applyAction(result);
-            }}
-            class="space-y-2"
-        >
-            <Button type="submit" disabled={oauthResult.success === "true"} class="w-[95%]">
-                <img src="/icons/services/google.png" alt="google service" class="mr-2 h-4 w-4" />
-                {$LL.area.oauth.action()}
-            </Button>
-            {#if oauthResult.service}
-                {#if oauthResult.success === "true"}
-                    <p class="text-center font-semibold text-sm text-green-500">Successfully connected to {serviceName(oauthResult.service)}</p>
-                {/if}
-                {#if oauthResult.success === "false"}
-                    <p class="text-center font-semibold text-sm text-red-500">Could not connect to {serviceName(oauthResult.service)}</p>
-                {/if}
-            {/if}
-            {#if form?.oauthErrorMessage}
-                <p class="text-center text-sm text-red-500">{form?.oauthErrorMessage}</p>
-            {/if}
-        </form>
-    {:else if action.auth === "apiKey"}
-        <p>API Key</p>
-    {:else if action.auth === "webhook"}
-        <p>Webhook</p>
-    {/if}
-    <!-- TODO: reaction auth (and other payload keys) -->
-    <div class="py-4">
+<ScrollArea class="max-h-[500px]">
+    <AREActionSelection
+        title="Action"
+        description={actions[actionId]?.description}
+        choices={actionsChoices}
+        id={actionId}
+        setId={value => {
+            actionId = value;
+            sessionStorage.setItem("actionId", actionId);
+        }}
+        service={actionService}
+        oauthCredentialChoices={actionOAuthCredentialChoices}
+        oauthId={oauthIds.action}
+        setOAuthId={value => {
+            oauthIds.action = value;
+            sessionStorage.setItem("actionOAuthId", oauthIds.action);
+        }}
+        oauthScopes={actions[actionId]?.oauthScopes}
+        setLastUpdated={() => setLastUpdated("action")}
+        oauthResult={lastUpdated === "action" ? oauthResult : undefined}
+        form={lastUpdated === "action" ? form : undefined}
+    />
+    <AREActionSelection
+        title="REAction"
+        description={reactions[reactionId]?.description}
+        choices={reactionsChoices}
+        id={reactionId}
+        setId={value => {
+            reactionId = value;
+            sessionStorage.setItem("reactionId", reactionId);
+        }}
+        service={reactionService}
+        oauthCredentialChoices={reactionOAuthCredentialChoices}
+        oauthId={oauthIds.reaction}
+        setOAuthId={value => {
+            oauthIds.reaction = value;
+            sessionStorage.setItem("reactionOAuthId", oauthIds.reaction);
+        }}
+        oauthScopes={reactions[reactionId]?.oauthScopes}
+        setLastUpdated={() => setLastUpdated("reaction")}
+        oauthResult={lastUpdated === "reaction" ? oauthResult : undefined}
+        form={lastUpdated === "reaction" ? form : undefined}
+    />
+    {#if actions[actionId] && reactions[reactionId]}
         <Separator />
-    </div>
-    <form method="POST" use:enhance class="grid gap-4">
-        <div class="flex w-[95%] max-w-sm flex-col gap-1.5">
-            <Label for="name">Name</Label>
-            <Input type="text" id="name" placeholder="Name" />
-        </div>
-        <div class="flex w-[95%] max-w-sm flex-col gap-1.5">
-            <Label for="description">Description</Label>
-            <Input type="text" id="description" placeholder="Description" />
-        </div>
-        <Separator />
-        <div class="space-y-4">
-            <p class="text-xs">
-                Here are the list of fields you can use for <strong>{action.name}</strong>.<br />
-                You can use them in the fields below to customize <strong>{reaction.name}</strong>.<br />
-                To use them, type <strong>{"{{<field_name>}}"}</strong> in the field.<br />
-                For example, to set a combination of the video title and channel name in the Embed title,
-                type <strong>{"{{title}} by {{channelName}}"}</strong> in the <strong>Title</strong> field below.
-            </p>
+        <!-- TODO: other payload keys -->
+        <div class="py-4">
             <Separator />
-            {#if FIELDS[reactionId]}
-                {#each FIELDS[reactionId] as field}
-                    <div class="flex w-[95%] max-w-sm flex-col gap-1.5">
-                        <Label for={field.name}>{field.displayName}</Label>
-                        <Input type={field.type} id={field.name} placeholder={field.displayName} />
-                        <p class="text-muted-foreground text-sm">{field.description}</p>
-                    </div>
-                {/each}
-            {/if}
         </div>
-        {#if action.auth !== "webhook"}
+        <form method="POST" use:enhance class="grid gap-4">
+            <div class="flex w-[95%] max-w-sm flex-col gap-1.5">
+                <Label for="name">Name</Label>
+                <Input type="text" id="name" placeholder="Name" />
+            </div>
+            <div class="flex w-[95%] max-w-sm flex-col gap-1.5">
+                <Label for="description">Description</Label>
+                <Input type="text" id="description" placeholder="Description" />
+            </div>
+            <Separator />
+            <div class="space-y-4">
+                <p class="text-xs">
+                    Here are the list of fields you can use for <strong>{actions[actionId].name}</strong>.<br />
+                    You can use them in the fields below to customize <strong>{reactions[reactionId].name}</strong>.<br />
+                    To use them, type <strong>{"{{<field_name>}}"}</strong> in the field.<br />
+                    For example, to set a combination of the video title and channel name in the Embed title,
+                    type <strong>{"{{title}} by {{channelName}}"}</strong> in the <strong>Title</strong> field below.
+                </p>
+                <Separator />
+                {#if FIELDS[reactionId]}
+                    {#each FIELDS[reactionId] as field}
+                        <div class="flex w-[95%] max-w-sm flex-col gap-1.5">
+                            <Label for={field.name}>{field.displayName}</Label>
+                            <Input type={field.type} id={field.name} placeholder={field.displayName} />
+                            <p class="text-muted-foreground text-sm">{field.description}</p>
+                        </div>
+                    {/each}
+                {/if}
+            </div>
             <Separator />
             <div class="flex w-[95%] max-w-sm flex-col gap-1.5">
                 <Label for="delay">Delay</Label>
                 <Input type="number" id="delay" placeholder="Delay" />
                 <p class="text-muted-foreground text-sm">The delay (in seconds) between each poll to the service.</p>
             </div>
-        {/if}
-    </form>
+            <Separator />
+            <div>
+                <div class="flex w-[95%] flex-row items-center justify-between">
+                    <Label for="enabled">Enable AREA</Label>
+                    <Switch id="enabled" />
+                </div>
+                <p class="text-muted-foreground text-sm">Enable or disable the AREA.</p>
+            </div>
+            <Separator />
+            <Button type="submit">
+                {$LL.area.createArea()}
+            </Button>
+        </form>
+    {/if}
 </ScrollArea>
