9f36862ca945d37f4ebb8ef6001edd78f6c91c6b
DURAND Malo <malo.durand@epitech.eu>
2024-11-03 23:30:39 +0100

    fix(backend): eexporting correctly reddit areas

--- apps/backend/src/area/services/reddit/reddit.transformers.spec.ts
+++ apps/backend/src/area/services/reddit/reddit.transformers.spec.ts
@@ -2,6 +2,6 @@ describe("Reddit transformers", () => {
     describe("transformer", () => {
         it("should pass", () => {
             expect(1).toBe(1);
-        })
-    })
-})
+        });
+    });
+});
