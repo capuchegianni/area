32b558a404e451b3d792803cbea2ebae6d989690
Zowks <67444066+zowks@users.noreply.github.com>
2024-11-12 23:56:00 +0100

    fix(merge)

--- apps/backend/src/scheduler/scheduler.service.ts
+++ apps/backend/src/scheduler/scheduler.service.ts
@@ -174,7 +174,7 @@ export class SchedulerService implements OnModuleInit, OnModuleDestroy {
 
         if (
             null === oldCache ||
-            data.cacheValue === parsedOldCache ||
+            data.cacheValue === oldCache ||
             null === transformedData
         )
             return true;
